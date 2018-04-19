0e26ce1c6be2632ae0eecc716c87b83520e12b50
Phil <gentrs80@gmail.com>
2018-04-18 17:12:16 -0700

    controller: reference index.blade.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,15 +9,10 @@ class PagesController extends Controller
     //Create a method (Or a function within a class)
 
     public function index() {
-        return 'INDEX';
-    }
- 
- 
- /*
-    public function index() {
         return view('pages.index');
     }
 
+ /*
     public function about() {
         return view('pages.about');
     }
