619062801b4e801ffabbc9e5214935fd774644c2
Phil <gentrs80@gmail.com>
2018-04-18 18:11:55 -0700

    page controller: index, about services

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,19 +6,19 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //Create a method (Or a function within a class)
+    //Create a method (Or a function within a class )
 
     public function index() {
         return view('pages.index');
     }
 
- /*
+
     public function about() {
         return view('pages.about');
     }
-    
+ 
     public function services() {
         return view('pages.services');
     }
- */   
+    
 }
