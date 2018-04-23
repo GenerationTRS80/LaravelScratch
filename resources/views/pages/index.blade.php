4cb5197a39c2232fc34aedc033ca92d1ae46cde2
Phil <gentrs80@gmail.com>
2018-04-22 17:02:41 -0700

    Add: Login and Reset buttons

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="jumbotron text-center">
     <h1> {{$title}}</h1>
     <p> This is the laravel application from the "Laravel from Scratch" Youtube series</p>
+    <p><a class="brt btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
 @endsection
