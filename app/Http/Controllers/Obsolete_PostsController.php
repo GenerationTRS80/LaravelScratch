74a2fa1e154fd26e24a2011937537ecd047da4c4
Phil <gentrs80@gmail.com>
2018-04-23 16:25:00 -0700

    Renamed: PostController to Obsolete PostControlder

--- app/Http/Controllers/Obsolete_PostsController.php
+++ app/Http/Controllers/Obsolete_PostsController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    // Resource methods
+    index
+    create
+    store
+    edit
+    update
+    show
+    destroy
 }
