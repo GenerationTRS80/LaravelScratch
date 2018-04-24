8edc624f48d1f277452b5b521480e38e6edba06a
Phil <gentrs80@gmail.com>
2018-04-23 18:39:26 -0700

    add comments April 23 18

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /*
+    //Change table Name 
+    protected $table = 'posts';
+
+    // Change Primary key field
+    public $primaryKey = 'posts_key';
+
+    // Add Timestamps
+    public $timestamp = true;
+    */
+
 }
