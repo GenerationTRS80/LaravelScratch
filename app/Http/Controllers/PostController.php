55c89eb82816aeb7622d00a23f4a9d3354cf3e6c
Phil <gentrs80@gmail.com>
2018-04-24 11:26:03 -0700

    add: Show post on DB with Show  page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -51,7 +51,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        // return Post::find($id);
+        $post = Post::find($id);
+        return view('posts.show')->with('post',$post);
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Posts</h1>
-    @if(count($posts)>1)
+  <a href="/posts" class="btn btn-default">Go Back</a>
+  <h1>{{$post->title}}</h1>
+    <div>
+      {{$post->body}}
+    </div>
+    <hr>
+    <small>Written on {{$post->created_at}}</small>
+
+    {{-- @if(count($posts)>1)
       @foreach($posts as $post)
           <div class="well">
             <h3><a href="/posts/{{$post->posts_key}}">{{$post->title}}</a></h3>
@@ -11,5 +18,5 @@
       @endforeach
     @else
       <p>No posts found</p>
-    @endif
+    @endif --}}
 @endsection
