f73d002fe5d267abc07aae03b172c1578b08d0b3
Phil <gentrs80@gmail.com>
2018-04-24 14:28:57 -0700

    remove: commented sectioin

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,14 +9,4 @@
     <hr>
     <small>Written on {{$post->created_at}}</small>
 
-    {{-- @if(count($posts)>1)
-      @foreach($posts as $post)
-          <div class="well">
-            <h3><a href="/posts/{{$post->posts_key}}">{{$post->title}}</a></h3>
-            <small>Written on {{$post->created_at}}</small>
-          </div>
-      @endforeach
-    @else
-      <p>No posts found</p>
-    @endif --}}
 @endsection
