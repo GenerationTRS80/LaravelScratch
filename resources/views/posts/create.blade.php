84f1698b28fedacd17d799f7afc7e7283a4071b2
Phil <gentrs80@gmail.com>
2018-04-28 14:39:34 -0700

    error: fixed

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
   <h1>Create Post</h1>
 
   
-  {!! Form::open(['action' => 'PostController@store', 'method' => 'POST']) !!}
+  {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
     <div class="form-group">
       {{-- Title --}}
       {{Form::label('title','Title')}}
