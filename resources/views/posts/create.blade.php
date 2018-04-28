a38a0eab39d7c84e94d28868507ad86554b4c81e
Phil <gentrs80@gmail.com>
2018-04-28 13:01:53 -0700

    add: Create Post form

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+{{-- Use Laravel Collective https://laravelcollective.com/docs/5.4/html to use as a HTML form Helper--}}
+@section('content')
+  <h1>Create Post</h1>
+
+@endsection
