4e5a78b93022d4351610ab3fa30937a79d8d939e
Phil <gentrs80@gmail.com>
2018-04-29 16:14:24 -0700

    add: Error checking with messages

---
+++ resources/views/inc/messages.blade.php
@@ -0,0 +1,23 @@
+
+{{-- Check 3 things: 1) Errors array 2) Session success 3) Session error failure--}}
+@if(count($errors)>0)
+  @foreach($errors->all() as $error)
+    <div class="alert alert-danger">
+      {{$error}}
+    </div>
+  @endforeach
+@endif
+
+{{-- Check Sessions --}}
+@if(session('success'))
+    <div class="alert alert-success">
+      {{session('success')}}
+    </div>
+@endif
+
+
+@if(session('success'))
+    <div class="alert alert-danger">
+      {{session('error')}}
+    </div>
+@endif
