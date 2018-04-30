a305605a496a2457672fe2913fb793b357b7a2a5
Phil <gentrs80@gmail.com>
2018-04-30 15:41:30 -0700

    add; parsing fot HTML in body

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,9 @@
   <a href="/posts" class="btn btn-default">Go Back</a>
   <h1>{{$post->title}}</h1>
     <div>
-      {{$post->body}}
+      {{-- Parse HTML with double exclamation !! --}}
+      {{-- When using the CheckEditor in the Create Post body, it will use HTML tags in the formatting of the text --}}
+      {!!$post->body!!}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
