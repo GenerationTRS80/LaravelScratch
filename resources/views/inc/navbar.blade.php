b117019dcb3f16b6a608132328433ad41d867bca
Phil <gentrs80@gmail.com>
2018-05-08 21:33:31 -0700

    add:  init authentication

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-inverse">
+{{-- <nav class="navbar navbar-inverse"> --}}
   {{--Remove from nav class above: navbar-fixed-top --}}
-    <div class="container">
+  {{--  <div class="container">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
           <span class="sr-only">Toggle navigation</span>
@@ -22,4 +22,64 @@
         </ul>
       </div><!--/.nav-collapse -->
     </div>
+  </nav> --}}
+
+  <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
+              <span class="sr-only">Toggle Navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+          </button>
+
+          {{-- Brand Image --}}
+          <a class="navbar-brand" href="{{ url('/') }}">
+              {{ config('app.name', 'Laravel') }}
+          </a>
+        </div>
+
+          <div class="collapse navbar-collapse" id="app-navbar-collapse">
+              <!-- Left Side Of Navbar -->
+              <ul class="navbar-nav">
+                &nbsp;
+              </ul>
+
+              <ul class="nav navbar-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/posts">Blog</a></li>
+              </ul>
+
+              <!-- Right Side Of Navbar -->
+              <ul class="nav navbar-nav navbar-right">
+                  <!-- Authentication Links -->
+                  @guest
+                      <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                      <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                  @else
+                      <li class="nav-item dropdown">
+                          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              {{ Auth::user()->name }} <span class="caret"></span>
+                          </a>
+
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="{{ route('logout') }}"
+                                 onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                                  {{ __('Logout') }}
+                              </a>
+
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                  @csrf
+                              </form>
+                          </div>
+                      </li>
+                  @endguest
+              </ul>
+          </div>
+      </div>
   </nav>
