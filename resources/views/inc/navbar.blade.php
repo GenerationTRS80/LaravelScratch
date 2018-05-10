49a85aa426fa1a3c7ef512ae4353387134fec6d0
Phil <gentrs80@gmail.com>
2018-05-09 19:20:28 -0700

    clean up:  comment and spaces

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,31 +1,31 @@
 <nav class="navbar navbar-inverse">
     <div class="container">
-    <div class="navbar-header">
+        <div class="navbar-header">
 
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
-            <span class="sr-only">Toggle Navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-        {{-- Brand Image --}}
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
-    </div>
+            {{-- Brand Image --}}
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+        </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-            &nbsp;
+                &nbsp;
             </ul>
 
             <ul class="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/services">Services</a></li>
-            <li><a href="/posts">Blog</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/posts">Blog</a></li>
             </ul>
 
             <!-- Right Side Of Navbar -->
